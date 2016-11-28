19b864946eb7cb423c1aa10b1bf795f8fff3f748
Sean Fraser <sean@plankdesign.com>
2016-11-28 09:50:08 -0500

    removed unused use statement

--- src/Media.php
+++ src/Media.php
@@ -2,7 +2,6 @@
 
 namespace Plank\Mediable;
 
-use Plank\Mediable\Exceptions\MediaUrlException;
 use Plank\Mediable\Helpers\File;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
