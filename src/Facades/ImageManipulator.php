5046b27732e2586d557add3dd173ba791da3d55b
Sean Fraser <frasmage@gmail.com>
2021-04-29 22:01:17 -0400

    cleanup facade PHPDOC

--- src/Facades/ImageManipulator.php
+++ src/Facades/ImageManipulator.php
@@ -10,7 +10,7 @@ use Plank\Mediable\Media;
  * @method static defineVariant(string $variantName, ImageManipulation $manipulation)
  * @method static bool hasVariantDefinition(string $variantName)
  * @method static ImageManipulation getVariantDefinition(string $variantName)
- * @method static Media createImageVariant(Media $media, string $variantName)
+ * @method static Media createImageVariant(Media $media, string $variantName, bool $forceRecreate = false)
  * @method static validateMedia(Media $media)
  */
 class ImageManipulator extends Facade
