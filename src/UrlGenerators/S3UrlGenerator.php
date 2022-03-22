fb6b1ee5f332350626660399393486578bb2ff23
Sean Fraser <frasmage@gmail.com>
2022-03-21 23:01:45 -0400

    Use new method in Laravel 9 for generating temporary S3 URLs

--- src/UrlGenerators/S3UrlGenerator.php
+++ src/UrlGenerators/S3UrlGenerator.php
@@ -3,9 +3,12 @@ declare(strict_types=1);
 
 namespace Plank\Mediable\UrlGenerators;
 
+use Aws\S3\S3Client;
+use Aws\S3\S3ClientInterface;
 use Illuminate\Contracts\Config\Repository as Config;
 use Illuminate\Contracts\Filesystem\Cloud;
 use Illuminate\Filesystem\FilesystemManager;
+use Illuminate\Support\Arr;
 use Plank\Mediable\Helpers\File;
 
 class S3UrlGenerator extends BaseUrlGenerator implements TemporaryUrlGeneratorInterface
@@ -47,8 +50,16 @@ class S3UrlGenerator extends BaseUrlGenerator implements TemporaryUrlGeneratorIn
 
     public function getTemporaryUrl(\DateTimeInterface $expiry): string
     {
+        $filesystem = $this->filesystem->disk($this->media->disk);
+
+        // Laravel 9+ / Flysystem 2+
+        if (method_exists($filesystem, 'temporaryUrl')) {
+            return $filesystem->temporaryUrl($this->media->getDiskPath(), $expiry);
+        }
+
+        // Earlier versions
         $root = config("filesystems.disks.{$this->media->disk}.root", '');
-        $adapter = $this->filesystem->disk($this->media->disk)->getDriver()->getAdapter();
+        $adapter = $filesystem->getDriver()->getAdapter();
         $command = $adapter->getClient()->getCommand(
             'GetObject',
             [
