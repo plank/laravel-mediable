0caaab50856143ce21828615be9aa1aee52a4c2c
anil kumar thakur <anilkumarthakur60@gmail.com>
2023-12-07 12:42:04 +0545

    styleci fixes

--- migrations/2016_06_27_000000_create_mediable_tables.php
+++ migrations/2016_06_27_000000_create_mediable_tables.php
@@ -39,12 +39,9 @@ class CreateMediableTables extends Migration
                     $table->foreignIdFor(Media::class)->constrained('media')->cascadeOnDelete();
                     $table->morphs('mediable');
                     $table->string('tag')->index();
-
-
                     $table->unsignedInteger('order')->index();
                     $table->primary(['media_id', 'mediable_type', 'mediable_id', 'tag']);
                     $table->index(['mediable_id', 'mediable_type']);
-
                 }
             );
         }
