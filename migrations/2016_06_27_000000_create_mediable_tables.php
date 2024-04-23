65bfc9dc6853af25874049d4fd858d26d95e9c4b
anil kumar thakur <anilkumarthakur60@gmail.com>
2024-04-24 03:14:20 +0545

    style-ci fixes

--- migrations/2016_06_27_000000_create_mediable_tables.php
+++ migrations/2016_06_27_000000_create_mediable_tables.php
@@ -5,9 +5,7 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 use Plank\Mediable\Media;
 
-//class CreateMediableTables extends Migration
 return new class extends Migration
-
 {
     /**
      * Run the migrations.
